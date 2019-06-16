62d41dc1078c8740945902ebfec9f71d2715f5ea
abbubhai <afrasat@unomaha.edu>
2019-06-16 04:15:01 -0500

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { TodoComponent } from "./todo/todo.component";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { Routes, RouterModule } from "@angular/router";
+
+const routes: Routes = [
+  { path: ":status", component: TodoComponent },
+  { path: "**", redirectTo: "/all" }
+];
+@NgModule({
+  declarations: [AppComponent, TodoComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
